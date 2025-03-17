c63689709cf3fd8cb072a549673bb9baf8782417
Genaro Lopez Chialva <genarolopezchialva@gmail.com>
2025-03-17 15:32:59 -0300

    cambio de genaro star

--- project/script.js
+++ project/script.js
@@ -1,3 +1,3 @@
 document.getElementById("myButton").addEventListener("click", function() {
-    document.getElementById("message").innerText = "Hola Mundo";
+    document.getElementById("message").innerText = "Hola Git";
 });
